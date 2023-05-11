92e3d75809e3f760b4465c044ca6a48fe2b6970b
Ilona Bozhe <warmte00@gmail.com>
2023-05-11 19:48:40 +0300

    first commit

---
+++ src/main/java/me/warmte/eventsourcing/EventSourcingApplication.java
@@ -0,0 +1,13 @@
+package me.warmte.eventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingApplication.class, args);
+	}
+
+}
