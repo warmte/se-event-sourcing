97585cbdc49a3f9d650787147c6aecadd90a6f97
Ilona Bozhe <warmte00@gmail.com>
2023-05-12 13:45:15 +0300

    tests

--- src/test/java/me/warmte/eventsourcing/EventSourcingApplicationTests.java
+++ src/test/java/me/warmte/eventsourcing/EventSourcingApplicationTests.java
@@ -31,18 +31,19 @@ class EventSourcingApplicationTests {
     private CheckpointService checkpointService;
 
     @Test
-    public void invalid_manager_create() {
+    public void managerCreateInvalid() {
         Model model = new ExtendedModelMap();
         String page = managerController.createPage("23,59,31,12,20230", model);
         assertThat(page).isEqualTo("ManagerPage");
     }
 
     @Test
-    public void valid_manager_create() {
+    public void managerCreateValid() {
         Model model1 = new ExtendedModelMap();
         String page1 = managerController.createPage("23,59,31,12,2023", model1);
         assertThat(page1).isEqualTo("CreatePage");
         long id = Long.parseLong((String) Objects.requireNonNull(model1.getAttribute("subscriptionId")));
+
         Model model2 = new ExtendedModelMap();
         String page2 = managerController.infoPage(id, model2);
         assertThat(page2).isEqualTo("InfoPage");
@@ -51,18 +52,78 @@ class EventSourcingApplicationTests {
     }
 
     @Test
-    public void manager_info() {
+    public void managerProlongValid() {
+        Model model1 = new ExtendedModelMap();
+        String page1 = managerController.createPage("23,59,31,12,2023", model1);
+        assertThat(page1).isEqualTo("CreatePage");
+        long id = Long.parseLong((String) Objects.requireNonNull(model1.getAttribute("subscriptionId")));
+
+        Model model2 = new ExtendedModelMap();
+        String page2 = managerController.prolongPage(id, "23,59,31,12,2024", model2);
+        assertThat(page2).isEqualTo("ProlongPage");
+
+        Model model3 = new ExtendedModelMap();
+        String page3 = managerController.infoPage(id, model3);
+        assertThat(page3).isEqualTo("InfoPage");
+        assertThat(model3.getAttribute("expiryDate"))
+                .isEqualTo(managerService.convert("23,59,31,12,2023").toString() + ", " + managerService.convert("23,59,31,12,2024").toString() + ", ");
     }
 
     @Test
-    public void manager_prolong() {
+    public void managerProlongInvalid() {
+        Model model1 = new ExtendedModelMap();
+        String page1 = managerController.createPage("23,59,31,12,2023", model1);
+        assertThat(page1).isEqualTo("CreatePage");
+        long id = Long.parseLong((String) Objects.requireNonNull(model1.getAttribute("subscriptionId")));
+
+        Model model2 = new ExtendedModelMap();
+        String page2 = managerController.prolongPage(id, "23,59,31,12,2022", model2);
+        assertThat(page2).isEqualTo("ManagerPage");
+
+        Model model3 = new ExtendedModelMap();
+        String page3 = managerController.infoPage(id, model3);
+        assertThat(page3).isEqualTo("InfoPage");
+        assertThat(model3.getAttribute("expiryDate"))
+                .isEqualTo(managerService.convert("23,59,31,12,2023").toString() + ", ");
     }
 
     @Test
-    public void checkpoint_login() {
+    public void checkpointLogin() {
+        Model model1 = new ExtendedModelMap();
+        String page1 = managerController.createPage("23,59,31,12,2023", model1);
+        assertThat(page1).isEqualTo("CreatePage");
+        long id = Long.parseLong((String) Objects.requireNonNull(model1.getAttribute("subscriptionId")));
+
+        Model model2 = new ExtendedModelMap();
+        String page2 = checkpointController.loginPage(id, model2);
+        assertThat(page2).isEqualTo("LoginPage");
     }
 
     @Test
-    public void checkpoint_logout() {
+    public void checkpointLogoutValid() {
+        Model model1 = new ExtendedModelMap();
+        String page1 = managerController.createPage("23,59,31,12,2023", model1);
+        assertThat(page1).isEqualTo("CreatePage");
+        long id = Long.parseLong((String) Objects.requireNonNull(model1.getAttribute("subscriptionId")));
+
+        Model model2 = new ExtendedModelMap();
+        String page2 = checkpointController.loginPage(id, model2);
+        assertThat(page2).isEqualTo("LoginPage");
+
+        Model model3 = new ExtendedModelMap();
+        String page3 = checkpointController.logoutPage(id, model3);
+        assertThat(page3).isEqualTo("LogoutPage");
+    }
+
+    @Test
+    public void checkpointLogoutInvalid() {
+        Model model1 = new ExtendedModelMap();
+        String page1 = managerController.createPage("23,59,31,12,2023", model1);
+        assertThat(page1).isEqualTo("CreatePage");
+        long id = Long.parseLong((String) Objects.requireNonNull(model1.getAttribute("subscriptionId")));
+
+        Model model3 = new ExtendedModelMap();
+        String page3 = checkpointController.logoutPage(id, model3);
+        assertThat(page3).isEqualTo("CheckpointPage");
     }
 }
